691210718f223e13e79794649bafc7dacf08c81a
Manoj <madhikari@power-guard.jp>
2024-05-20 16:13:42 +0900

    adding url for api

--- pg_solar_api/urls.py
+++ pg_solar_api/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
+         name='api-docs'),
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False), name='index'),
+    path('api/user/', include('user.urls')),
 ]
