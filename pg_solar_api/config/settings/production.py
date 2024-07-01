06dfb9bb0c11102772a4dea6a90c7f0a9dd905f9
root <root@DESKTOP-33BFR3H>
2024-07-01 17:34:52 +0900

    update production file

--- pg_solar_api/config/settings/production.py
+++ pg_solar_api/config/settings/production.py
@@ -7,14 +7,21 @@ SECRET_KEY = "This is the Production server."
 ALLOWED_HOSTS = ['127.0.0.1']
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': None,
+#         'NAME': None,
+#         'USER': None,
+#         'PASSWORD': None,
+#         'HOST': None,
+#         'PORT': None,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': None,
-        'NAME': None,
-        'USER': None,
-        'PASSWORD': None,
-        'HOST': None,
-        'PORT': None,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
